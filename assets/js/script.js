a0125a2d3a36b41310dcfdb7abe88ebd9776cb7d
angelo-lepore <angelo.lepore94@gmail.com>
2025-06-11 18:07:26 +0200

    finished-exercise-to-fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,3 +37,25 @@ const teamMembers = [
   },
 ];
 console.log(teamMembers);
+
+const container = document.querySelector(".row");
+teamMembers.forEach((member) => {
+  const card = document.createElement("div");
+  card.classList.add("col", "mb-4");
+
+  card.innerHTML = `
+<div class="card mb-3">
+  <div class="row bg-dark text-white">
+    <div class="col-md-4 p-0">
+        <img src="./assets/${member.img}" class="card-img" alt="${member.name}">
+      </div>
+      <div class="col-md-8">
+        <p class="card-name m-0 fw-bold">${member.name}</p>
+        <p class="card-role">${member.role}</p>
+        <p class="card-email text-primary">${member.email}</p>
+      </div>
+    </div>
+  </div>
+  `;
+  container.appendChild(card);
+});
